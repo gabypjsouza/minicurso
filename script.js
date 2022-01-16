a4ca6505f486e25a2417dd58dfa60a99d88ed328
Gabriela Souza <arq.gabrielasouza@gmail.com>
2022-01-15 21:23:35 -0300

    Capturar elementos html manipuláveis

---
+++ script.js
@@ -0,0 +1,5 @@
+const imagem = document.querySelector('img');
+const botão = document.querySelector('button');
+const nomeDoPersonagem = document.querySelector('#nome');
+const especie = document.querySelector('#especie');
+const status = document.querySelector('#status');
